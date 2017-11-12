7ed3ffd9f5b153368c03c0d98afe376e72c2c488
jd <jose4125@gmail.com>
2017-11-12 09:03:18 -0500

    add router and redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import createHistory from 'history/createBrowserHistory';
+import { Route } from 'react-router';
+import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux';
+
 import './index.css';
-import App from './App';
+import reducers from './reducers';
+import HOME from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const HISTORY = createHistory();
+const MIDDLEWARE = routerMiddleware(HISTORY);
+const STORE = createStore(
+  combineReducers({
+    ...reducers,
+    router: routerReducer,
+  }),
+  applyMiddleware(MIDDLEWARE),
+);
+
+ReactDOM.render(
+  <Provider store={STORE}>
+    <ConnectedRouter history={HISTORY}>
+      <div>
+        <Route exact path="/" component={HOME} />
+      </div>
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root'),
+);
+/*
+ReactDOM.render(
+  <App />, document.getElementById('root')
+);
+
+*/
 registerServiceWorker();
