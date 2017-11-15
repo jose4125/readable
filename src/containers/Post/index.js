e3a96534c61a62497ee4c815e3695a25de9f195d
jd <jose4125@gmail.com>
2017-11-14 22:41:25 -0500

    date readable format

--- src/containers/Post/index.js
+++ src/containers/Post/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import moment from 'moment';
 
 import { fetchPost, deletePost } from './actions';
 import Comments from '../Comments';
@@ -22,12 +23,13 @@ class Post extends React.PureComponent {
 
   render() {
     const { title, author, timestamp, voteScore, body, id } = this.props.post;
+    const date = moment(timestamp).format('ll');;
     return (
       <div className="App">
         <header className="App-header">
           <h2>{title}</h2>
           <p>author: {author}</p>
-          <p>date: {timestamp}</p>
+          <p>date: {date}</p>
           <p>score: {voteScore}</p>
           <Link to={`/post/${id}/edit`}>EDIT POST</Link>
           <span> | </span>
